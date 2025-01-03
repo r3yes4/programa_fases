af622c173d863828a6d6dc7e6de4202fb9896cdf
Carnisser <168170317+Carnisser@users.noreply.github.com>
2025-01-03 14:36:13 +0100

    file_upload_virustotal
    funciona pero no esta bonito

---
+++ completo/html/check_virus.py
@@ -0,0 +1,138 @@
+import os
+import requests
+import json
+import mimetypes
+import time
+import shutil
+import hashlib
+
+def obtenerArchivos(directorio):
+    archivos_encontrados = []
+    carpetas_a_omitir = {'infectados', 'limpios'}
+    
+    for root, dirs, files in os.walk(directorio):
+        # Omitir carpetas específicas
+        dirs[:] = [d for d in dirs if d not in carpetas_a_omitir]
+        
+        for filename in files:
+            ruta_completa = os.path.join(root, filename)  # Construir la ruta completa
+            archivos_encontrados.append(ruta_completa)  # Agregar la ruta a la lista
+    
+    return archivos_encontrados
+
+def calcular_sha256(ruta_archivo):
+    sha256_hash = hashlib.sha256()
+    with open(ruta_archivo, "rb") as archivo:
+        for bloque in iter(lambda: archivo.read(4096), b""):
+            sha256_hash.update(bloque)
+    return sha256_hash.hexdigest()
+
+# Obtener archivos del directorio
+archivos = obtenerArchivos("uploads")
+
+api_key = "890d64820c761129bf48777e0182b612a1acfb590b04045171faff730633d686"
+
+if archivos:
+    for archivo in archivos:
+        file_hash = calcular_sha256(archivo)
+        if os.path.exists("history.json"):
+            with open("history.json", "r") as history:
+                data = json.load(history)
+
+            for item in data["logs"]:
+                if item["hash"] == file_hash:
+                    if item["virus"] == False:
+                        shutil.move(archivo, "archivos/limpios")
+                        exit()
+                    
+        # Inicio de la petición API
+        mime_type, _ = mimetypes.guess_type(archivo)
+        stat = os.stat(archivo)
+        umbral = 32 * 1024 * 1024
+        umbral2 = 650 * 1024 * 1024
+        if stat.st_size > umbral2:
+            exit()
+        if stat.st_size < umbral:
+            url = "https://www.virustotal.com/api/v3/files"
+        else:
+            url = "https://www.virustotal.com/api/v3/files/upload_url"
+
+            headers = {
+                "accept": "application/json",
+                "x-apikey": "890d64820c761129bf48777e0182b612a1acfb590b04045171faff730633d686"
+            }
+
+            response = requests.get(url, headers=headers)
+            data = json.loads(response.text)
+            url = data["data"]
+
+
+
+
+        with open(archivo, "rb") as file_data:  # Asegurarse de cerrar el archivo después
+            files = { "file": (archivo, file_data, mime_type) }
+            headers = {
+                "accept": "application/json",
+                "x-apikey": api_key
+            }
+
+            response = requests.post(url, files=files, headers=headers)
+
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                file_id = data["data"]["id"]
+            else:
+                print(f"Error al enviar {archivo}: {response.status_code} - {response.text}")
+        # Fin de la petición API upload
+
+        ### Verificar si tiene virus
+        #Inicio peticion API
+        url2 = "https://www.virustotal.com/api/v3/analyses/"+file_id
+
+        headers2 = {
+            "accept": "application/json",
+            "x-apikey": api_key
+        }
+
+        response = requests.get(url2, headers=headers2)
+        
+
+        json_data = response.json() 
+        #fin api req      
+        
+        attributes = json_data['data']['attributes']
+        results = attributes['results']
+        file_hash = json_data['meta']['file_info']['sha256']
+
+        virus = False
+        for key, value in results.items():
+            if value['result'] != None:
+                virus = True
+                
+        if virus == True:
+            os.remove(archivo)
+            print("1")
+        else:
+            shutil.move(archivo, "uploads/limpios")
+            print("0")
+        log= {
+            'hash' : file_hash,
+            'virus' : virus
+        }  
+
+        if os.path.exists("history.json"):
+            with open("history.json", "r") as history:
+                data = json.load(history)
+        else:
+            data = {"logs": []}
+        for item in data["logs"]:
+            if item["hash"] != log["hash"]:
+                data["logs"].append(log)
+                with open("history.json", "w") as history:
+                    json.dump(data, history, indent=3)
+        
+
+        time.sleep(20)
+    
+else:
+    print("No se han encontrado archivos")
--- completo/html/upload.php
+++ completo/html/upload.php
@@ -6,8 +6,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             mkdir($uploadDir, 0777, true);
         }
         $uploadFile = $uploadDir . basename($_FILES['file']['name']);
+    
         if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
-            echo "Archivo subido con éxito: " . $_FILES['file']['name'];
+            // Ejecutar el script de Python para verificar el archivo
+            $output = shell_exec("python3 check_virus.py");
+            if ($output === null) {
+                echo "Error al ejecutar el script de verificación.";
+                unlink($uploadFile);
+                exit;
+            }
+            $result = trim($output); // Eliminar espacios en blanco alrededor de la salida
+
+            // Verificar el resultado del script de Python
+            if ($result === '0') {
+                echo "Archivo subido con éxito: " . $_FILES['file']['name'];
+            } elseif ($result === '1') {
+                // Eliminar el archivo si es malicioso
+                
+                echo "El archivo es malicioso y ha sido eliminado.";
+            } else {
+                echo "Error al verificar el archivo.";
+            }
         } else {
             echo "Error al mover el archivo.";
         }
